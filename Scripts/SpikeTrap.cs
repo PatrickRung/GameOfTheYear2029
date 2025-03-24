e8e1f498685af04dc482a1bd75d3b7bb796d0d59
PatrickRung <96024158+PatrickRung@users.noreply.github.com>
2025-03-24 13:55:00 -0700

    Add Spike Trap Animations
    When player enters range, triggers spike trap animation. To do would be to implement delay to damage so that player can dodge trap

--- Scripts/SpikeTrap.cs
+++ Scripts/SpikeTrap.cs
@@ -4,8 +4,10 @@ using System;
 public partial class SpikeTrap : MeshInstance3D
 {
 	// Called when the node enters the scene tree for the first time.
+	private protected AnimationPlayer spikeAnimPlayer;
 	public override void _Ready()
 	{
+		spikeAnimPlayer = (AnimationPlayer)this.FindChild("AnimationPlayer");	
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,9 +18,11 @@ public partial class SpikeTrap : MeshInstance3D
 
 	public void _on_area_3d_body_entered(Node3D hit) {
 		GD.Print("entered spike range");
+		spikeAnimPlayer.Play("spikeEnable");
 		if(hit.Name.Equals("Player")) {
 			Player playerObject = (Player)hit;
 			playerObject.dealDamae(20);
+			
 		}
 	}
 }
